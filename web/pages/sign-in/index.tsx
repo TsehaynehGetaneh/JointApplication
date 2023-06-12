2ee8fb63d9f4fa5b3163960644f9fc1fcb362a2e
Tsehayneh Getaneh <tsigsg@gmail.com>
2023-06-12 21:46:47 +0300

    feat(tsehayneh) : Add signup and sign-in page

---
+++ web/pages/sign-in/index.tsx
@@ -0,0 +1,12 @@
+import SignIn from "@/components/sign-in/SignIn";
+import { NextPage } from "next";
+
+const SignInPage: NextPage = () => {
+    return (
+        <div>
+            <SignIn />
+        </div>
+    )
+}
+
+export default SignInPage;
