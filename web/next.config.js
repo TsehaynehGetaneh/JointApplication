ea61737634f94de5d199941b58be8fa74f6061f7
Tsehayneh Getaneh <tsigsg@gmail.com>
2023-06-18 22:51:04 +0300

    Add packages

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ['res.cloudinary.com', 'th.bing.com', 'i.pinimg.com','www.bing.com'],
   },
 };
