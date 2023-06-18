35017b7e33510da0a5a1e357f7ba70021b79bffa
Tsehayneh Getaneh <tsigsg@gmail.com>
2023-06-19 00:41:18 +0300

    feat(tsehayneh) : Add applicant college search

--- web/store/college/college-api.tsx
+++ web/store/college/college-api.tsx
@@ -7,12 +7,15 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const collegeApi = createApi({
   reducerPath: 'collegeApi',
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL, mode: 'cors' }),
+  tagTypes: ['College'],
   endpoints: (builder) => ({
     getColleges: builder.query<University[], void>({
       query: () => '/universities',
+      providesTags: ['College'],
     }),
     getCollegeById: builder.query<University, string>({
       query: (id) => `/universities/${id}`,
+      providesTags:(result, meta, args) => [{id: args, type:'College'}]
     }),
   }),
 });
