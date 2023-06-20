c1050863a0702c52b252d281f2ec51d640875672
addis <addeson3@gmail.com>
2023-06-20 12:02:36 -0700

    myColleges ctrl updates

--- backend/controllers/applications/applicationCtrl.js
+++ backend/controllers/applications/applicationCtrl.js
@@ -9,7 +9,7 @@ const applicationCtrl = async (req, res) => {
       // find  user
       const user = await User.findById(req.userAuth);
       // find college
-      const userCollege = req.params._id
+      const userCollege = req.params._id;
       //create applicaton
       const applicationCreated = await Application.create({
             user: user._id,
@@ -43,32 +43,4 @@ module.exports = {
      viewApplicationCtrl
      }; 
 
-    //  const Application = require('../../model/Application/Application');
-
-    //  // Create a new college application
-    //  const applicationCtrl = async (req, res) => {
-    //    try {
-    //      const newApplication = new Application(req.body);
-    //      await newApplication.save();
-    //      res.status(201).json(newApplication);
-    //    } catch (err) {
-    //      console.error(err);
-    //      res.status(500).json({ message: 'Server error' });
-    //    }
-    //  };
-     
-    //  // Get all college applications
-    //  const viewApplicationCtrl = async (req, res) => {
-    //    try {
-    //      const allApplications = await Application.find();
-    //      res.json(allApplications);
-    //    } catch (err) {
-    //      console.error(err);
-    //      res.status(500).json({ message: 'Server error' });
-    //    }
-    //  };
-     
-    //  module.exports = {
-    //    applicationCtrl,
-    //    viewApplicationCtrl
-    //  };   
+  
--- backend/model/MyColleges/MyColleges.js
+++ backend/model/MyColleges/MyColleges.js
@@ -3,30 +3,15 @@ const mongoose = require('mongoose');
 const userCollegeSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+    ref: "User",
     required: true
   },
-  firstName: {
-    type: String,
-    required: false
-  },
-  lastName: {
-    type: String,
-    required: false
-  },
-  email: {
-    type: String,
-    required: false
-  },
-  college: {
+  college:[
+     {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'University',
-    required: true
   },
-  collegeName: {
-    type: String,
-    required: false
-  }
+]
 });
 
 const UserCollege = mongoose.model('UserCollege', userCollegeSchema);
